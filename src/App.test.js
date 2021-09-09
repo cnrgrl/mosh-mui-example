f90f92665432b5d317369d24800d30a1a32f97c4
caner <caner@quizco.de>
2021-09-09 14:25:49 +0200

    test link

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/QuizCounter ® 2021/i);
   expect(linkElement).toBeInTheDocument();
 });
