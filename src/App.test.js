ccfaf43cc9f7a5c2dc82d9b6549249be8c340155
caner <caner@quizco.de>
2021-09-09 14:27:29 +0200

    describe it..

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/QuizCounter ® 2021/i);
-  expect(linkElement).toBeInTheDocument();
+describe("counter element", () => {
+  it("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/QuizCounter ® 2021/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
